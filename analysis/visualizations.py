01050f44c77d8e0926572cba011bca854e9b62a1
anshikaraj-hub <anshika.raj1606@gmail.com>
2026-06-11 23:53:53 +0530

    fix: interactive visualization added Land vs Ocean Comparison Chart

--- analysis/visualizations.py
+++ analysis/visualizations.py
@@ -2,6 +2,7 @@ from calendar import calendar
 
 import matplotlib.pyplot as plt
 import plotly.express as px
+import plotly.graph_objects as go
 import seaborn as sns
 
 import calendar
@@ -25,7 +26,7 @@ if __name__ == "__main__":
     top_countries = top_warming_countries(clean_country)
     monthly_avg, hottest, coldest = seasonal_analysis(clean_global)
 
-    '''
+    
     #chart 1: Global Land Average Temperature Trend
     plt.figure(figsize=(12,6))
     plt.plot(yearly_land['dt'], yearly_land['LandAverageTemperature'], color='red', linewidth=1)
@@ -50,7 +51,7 @@ if __name__ == "__main__":
     #chart 4: Decadal Temperature Changes
     fig = px.bar(decade_land, x='decade', y='LandAverageTemperature', title='Average Land Temperature by Decade', color='LandAverageTemperature', color_continuous_scale='Oranges', labels={'LandAverageTemperature': 'Average Temperature (°C)'})
     fig.show()
-    '''
+    
 
     #Chart 5: Seasonal Analysis
     monthly_avg['Month_Name'] = monthly_avg['dt'].apply(lambda x: calendar.month_name[int(x)])
@@ -58,4 +59,13 @@ if __name__ == "__main__":
                    title='Seasonal Temperature Pattern (Global Average)',
                    markers=True,
                    labels={'LandAverageTemperature': 'Average Temperature (°C)', 'Month_Name': 'Month'})
+    fig.show()
+    
+
+    #Chart 6: Land vs Ocean
+    yearly_land_and_ocean = calculate_yearly_averages(clean_global, "land_and_ocean")
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=yearly_land['dt'], y=yearly_land['LandAverageTemperature'], mode='lines', name='Land Only', line=dict(color='orange')))
+    fig.add_trace(go.Scatter(x=yearly_land_and_ocean['dt'], y=yearly_land_and_ocean['LandAndOceanAverageTemperature'], mode='lines', name='Land & Ocean', line=dict(color='blue')))
+    fig.update_layout(title='Global Land vs Land & Ocean Temperature Trends', xaxis_title='Year', yaxis_title='Average Temperature (°C)')
     fig.show()
